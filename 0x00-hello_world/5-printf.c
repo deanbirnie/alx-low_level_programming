e6a29a69551102d6eb468c402f033293c4f30f31
deanbirnie <deanicdl@gmail.com>
2023-02-16 07:29:50 -0800

    Task 5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints string to console.
+ *
+ * Return: string
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
