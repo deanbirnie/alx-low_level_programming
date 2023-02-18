961254fdab1861ce3d9624ae051b2eda623334b1
deanbirnie <deanicdl@gmail.com>
2023-02-18 01:44:13 -0800

    Task 8 base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(i + 'a');
+	}
+	putchar('\n');
+
+	return (0);
+}
