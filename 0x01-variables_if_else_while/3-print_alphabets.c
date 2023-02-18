81b7a19b4550f8512332bf22fac3b4e7294d7877
deanbirnie <deanicdl@gmail.com>
2023-02-17 23:59:50 -0800

    Task 3 alphABETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int l;
+	int u;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	for (u = 'A'; u <= 'Z'; u++)
+	{
+		putchar(u);
+	}
+	putchar('\n');
+
+	return (0);
+}
