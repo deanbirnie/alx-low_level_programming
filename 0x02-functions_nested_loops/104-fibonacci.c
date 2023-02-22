2cab8b722037e4de452daed2802bed6c63004b77
deanbirnie <deanicdl@gmail.com>
2023-02-22 13:38:01 -0800

    Task 16 fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,29 +8,33 @@
 int main(void)
 {
 	int i;
-	long int prev = 1, curr = 1, next;
-	long int prevH, prevL, currH, currL, nextH, nextL;
+	unsigned long int prev = 1, curr = 1, next;
+	unsigned long int prevH, prevL, currH, currL, nextH, nextL;
 
 	printf("%lu", curr);
-	for (i = 0; i < 92; i++)
+	for (i = 1; i < 91; i++)
 	{
 		next = prev + curr;
 		printf(", %ld", next);
 		prev = curr;
 		curr = next;
 	}
-	nextH = (prev / 1000000);
-	nextL = (prev % 1000000);
+	prevH = (prev / 1000000000);
+	prevL = (prev % 1000000000);
+	currH = (curr / 1000000000);
+	currL = (curr % 1000000000);
+	/*nextH = prevH + currH;*/
+	/*nextL = prevL + currL;*/
 
-	for (i = 91; i < 98; i++)
+	for (i = 92; i < 99; i++)
 	{
+		nextH = prevH + currH;
+		nextL = prevL + currL;
 		printf(", %lu%lu", nextH, nextL);
 		prevH = currH;
 		prevL = currL;
 		currH = nextH;
 		currL = nextL;
-		nextH = prevH + currH;
-		nextL = prevL + currL;
 	}
 	printf("\n");
 
