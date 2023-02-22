789bac521b4cfa6a33bb406ac36ecc4a6e1b3334
deanbirnie <deanicdl@gmail.com>
2023-02-22 01:07:38 -0800

    Task 9 Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			k = i * j;
+
+			if (k < 10)
+			{
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
