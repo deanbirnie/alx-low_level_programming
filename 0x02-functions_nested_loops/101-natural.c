d04d249aa97e3b1ddeac5d6bf51fde3d38d8fc86
deanbirnie <deanicdl@gmail.com>
2023-02-22 05:01:59 -0800

    Task 13 Natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j = j + i;
+		}
+	}
+	printf("%d\n", j);
+}
