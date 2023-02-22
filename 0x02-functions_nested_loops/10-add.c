792ac700e2a678b6fe90a78e5fb7560665fb7de7
deanbirnie <deanicdl@gmail.com>
2023-02-22 02:09:58 -0800

    Task 10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @i: first integer passed to function
+ * @j: second integer passed to function
+ *
+ * Return: void
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
