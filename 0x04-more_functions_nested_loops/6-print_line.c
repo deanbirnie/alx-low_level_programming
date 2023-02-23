c60e6abee0562efa1e1748cc918888c1fc33165b
deanbirnie <deanicdl@gmail.com>
2023-02-23 12:31:01 -0800

    Task 6 lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line of length n
+ *
+ * @n: integer passed to function
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
