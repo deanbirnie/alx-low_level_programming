d505c849e485a36ad9162e4f5df8b1a0af6716a6
deanbirnie <deanicdl@gmail.com>
2023-02-23 13:32:10 -0800

    Task 7 Diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonals
+ *
+ * @n: the number of time \ should be printed
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
