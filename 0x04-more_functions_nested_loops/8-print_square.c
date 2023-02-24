d26277d80c3f3aa080f7c54f5a68f8c5fe3b6f60
deanbirnie <deanicdl@gmail.com>
2023-02-23 19:27:41 -0800

    Task 8 Squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of dimensions 'size'
+ *
+ * @size: integer passed to function
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
