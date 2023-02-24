24de2f6e6a11321b652cd4d4bb83c38900e4c834
deanbirnie <deanicdl@gmail.com>
2023-02-23 20:26:52 -0800

    Task 10 Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle ofdimensions 'size'
+ *
+ * @size: integer passed to function
+ */
+void print_triangle(int size)
+{
+	if (size > 0 )
+	{
+		int i, j, k;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
