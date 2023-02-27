79a388ca463131ebabd85b6499928dd3973407bf
deanbirnie <deanicdl@gmail.com>
2023-02-27 13:07:24 -0800

    Task 6 Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints alternating characters of a string
+ *
+ * @str: pointer passed to function
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2)
+		{
+			_putchar(str[i - 1]);
+		}
+	}
+	_putchar('\n');
+}
