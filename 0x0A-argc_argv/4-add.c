69ad0441ac26195d29cb1c6d032cba89ca35b179
deanbirnie <deanicdl@gmail.com>
2023-03-10 13:15:45 -0800

    Task 4 Add Arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0 (success) OR 1 (error)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
