98d15f3ebb6fed2a34a7b656da8e58052e70a377
deanbirnie <deanicdl@gmail.com>
2023-03-20 14:47:41 -0700

    Task 3 Typedef for Struct Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - new name for type struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
