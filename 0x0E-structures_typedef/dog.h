eaac3dc761a601481842c51387190f463a976af0
deanbirnie <deanicdl@gmail.com>
2023-03-20 08:10:13 -0700

    Task 0 Structures - Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - information about a specific dog
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: Information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
