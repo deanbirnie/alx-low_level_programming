4f678a5978dfbb41516514bb4d0045d2531381b3
deanbirnie <deanicdl@gmail.com>
2023-03-22 04:13:12 -0700

    Fixed error received for struct dog in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,12 +21,14 @@ struct dog
 };
 
 /**
- * struct dog_t - new name for type struct dog
+ * dog_t - new name for type struct dog
  */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
