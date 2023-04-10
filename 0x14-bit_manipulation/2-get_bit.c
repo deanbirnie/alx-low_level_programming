6492f18f6c076b6510220e66c4736f3ecb10a2c0
deanbirnie <deanicdl@gmail.com>
2023-04-10 10:01:14 -0700

    Task 2 Value of bit and given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - takes an unsigned long int and finds a bit at
+ * a specified index value
+ *
+ * @n: value to be parsed
+ * @index: place value at which to find a bit
+ *
+ * Return: bit at given index or -1 if failure
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	i = (unsigned long int) 1 << index;
+
+	return ((n & i) >> index);
+}
