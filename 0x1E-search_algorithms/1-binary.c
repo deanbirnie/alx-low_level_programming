42af88883d68d85d0c3b52f20aad2a9940a6e2f4
deanbirnie <deanicdl@gmail.com>
2023-09-13 07:06:30 -0700

    Task 1 - Search for value in array using Binary Search Algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,63 @@
+#include "search_algos.h"
+
+/**
+ *
+ */
+void print_array(int *array, size_t size)
+{
+        size_t i;
+
+        printf("Searching in array: ");
+        for (i = 0; i < size; ++i)
+        {
+		if (i == 0)
+		{
+			printf("%d", array[i]);
+		} else
+		{
+			printf(", %d", array[i]);
+		}
+        }
+        printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers using
+ * the Binary Search Algorithm
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: the value to search for
+ *
+ * Return: index where value is located or -1 if array is NULL
+ * or the value is not present.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t middle;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (left <= right)
+	{
+		print_array(array + left, right - left + 1);
+		middle = left + (right - left) / 2;
+		if (array[middle] == value)
+		{
+			return (middle);
+		} else if (array[middle] < value)
+		{
+			left = middle + 1;
+		} else
+		{
+			right = middle - 1;
+		}
+	}
+
+	return (-1);
+}
